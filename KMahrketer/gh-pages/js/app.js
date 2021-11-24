429006842a33ed6511696236ed8552ea13f9d08f
ZakharAll <91776783+ZakharAll@users.noreply.github.com>
2021-11-24 11:33:52 +0700

    v 0.2
    new script for load page

--- KMahrketer/gh-pages/js/app.js
+++ KMahrketer/gh-pages/js/app.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
         $('.burger, .menu').toggleClass('active');
     });
 });
+
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    	$svg_anm.fadeOut();
+    	$preloader.delay(800).fadeOut('slow');
+});
