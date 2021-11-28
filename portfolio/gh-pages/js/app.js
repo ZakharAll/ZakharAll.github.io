3552371cc7ea0b044b4d17a97e59daa1dfbdfe90
ZakharAll <91776783+ZakharAll@users.noreply.github.com>
2021-11-28 22:10:49 +0700

    update version
    new version 1.0
    adaptive
    new js scripts

--- portfolio/gh-pages/js/app.js
+++ portfolio/gh-pages/js/app.js
@@ -5,12 +5,35 @@ $(document).ready(function(){
 		}else{
 			$('.navbar').removeClass("sticky");
 		}
+		if(this.scrollY > 500){
+			$('.scroll-up-btn').addClass("show");
+		}else{
+			$('.scroll-up-btn').removeClass("show");
+		}
+	});
+
+	// slide up script
+	$('.scroll-up-btn').click(function() {
+		$('html').animate({scrollTop:0});
 	});
 	// toggle menu navbar
 	$('.menu-btn').click(function() {
 		$('.navbar .menu').toggleClass("active");
 	});
 
+	// typing animation
+	let typed = new Typed(".typing",{
+		strings:[" Frontend Developer", " Freelancer", " Designer", " Blogger"],
+		typeSpeed:100, 
+		backSpeed:60,
+		loop:true
+	});
+	let typed2 = new Typed(".typing-2",{
+		strings:[" Frontend Developer", " Freelancer", " Designer", " Blogger"],
+		typeSpeed:100, 
+		backSpeed:60,
+		loop:true
+	});
 	// owl carousel script
 	$('.carousel').owlCarousel({
 		margin:20,
